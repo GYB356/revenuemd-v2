5e8b33d3c7d14c25289be58b56114c7039c14abf
GYB356 <olari6471@gmail.com>
2025-01-25 22:29:17 +0200

    Create supabase-client.ts

---
+++ supabase-client.ts
@@ -0,0 +1,13 @@
+import { createClient } from "@supabase/supabase-js"
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Missing environment variables for Supabase")
+}
+
+export const createClient = () => {
+  return createClient(supabaseUrl, supabaseAnonKey)
+}
+
